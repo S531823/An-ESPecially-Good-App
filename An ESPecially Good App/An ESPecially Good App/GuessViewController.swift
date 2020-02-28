61029296c7cc647b6f30a3f5bc3de980b6c2956d
Tommy Vo <s531823@nwmissouri.edu>
2020-02-28 17:56:25 -0600

    Add files via upload

---
+++ An ESPecially Good App/An ESPecially Good App/GuessViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  An ESPecially Good App
+//
+//  Created by S531823 on 2/25/20.
+//  Copyright © 2020 S531823. All rights reserved.
+//
+
+import UIKit
+
+class GuessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ An ESPecially Good App/An ESPecially Good App/StatisticsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  An ESPecially Good App
+//
+//  Created by S531823 on 2/25/20.
+//  Copyright © 2020 S531823. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
